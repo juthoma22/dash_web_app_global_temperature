9a4026da7f59aeeab6883d28590a4178849e4a48
juthoma22 <julius9819@hotmail.de>
2022-11-23 10:44:25 +0100

    web app done

---
+++ app.py
@@ -0,0 +1,80 @@
+import dash
+
+from dash.dependencies import Input, Output
+from dash import dcc
+from dash import html
+
+import data_handler as dh
+
+DataHandler = dh.DataHandler()
+
+df_country, years, countries = DataHandler.read_files()
+
+# Layout used later for the graph
+layout = dict(
+    title="Use slider to move through years",
+    geo=dict(
+        showframe=False,
+        showocean=True,
+        oceancolor="rgb(0,255,255)",
+        type="equirectangular",
+    ),
+)
+
+app = dash.Dash("Gloabal Temperature")
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Average Temperature per year in each country"),
+        dcc.Graph(
+            id="average-temp-year",
+        ),
+        html.Div(
+            dcc.Slider(
+                int(years.min()),
+                int(years.max()),
+                step=1,
+                id="year-slider",
+                value=int(years.max()),
+                marks={
+                    str(year): str(year)
+                    for year in [1750, 1800, 1850, 1900, 1950, 2000]
+                },
+            ),
+            style={"width": "95%", "padding": "0px 20px 20px 20px"},
+        ),
+    ]
+)
+
+
+@app.callback(Output("average-temp-year", "figure"), Input("year-slider", "value"))
+def update_graph(year):
+    # changing dt to just the years
+    dff = df_country[df_country["dt"].apply(lambda x: x[:4]) == str(year)]
+
+    mean_temp = []
+    for country in countries:
+        mean_temp.append(dff[dff["Country"] == country]["AverageTemperature"].mean())
+
+    # using the calculated mean temperature for each country
+    data = [
+        dict(
+            type="choropleth",
+            locations=countries,
+            z=mean_temp,
+            locationmode="country names",
+            text=countries,
+            marker=dict(line=dict(color="rgb(0,0,0)", width=1)),
+            colorbar=dict(
+                autotick=True, tickprefix="", title="Average\nTemperature,\n°C"
+            ),
+        )
+    ]
+
+    fig = dict(data=data, layout=layout)
+
+    return fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ data_handler.py
@@ -0,0 +1,69 @@
+import sys
+import kaggle
+import pandas as pd
+import numpy as np
+
+
+class DataHandler:
+    def __init__(self):
+        self.dataset = "berkeleyearth/climate-change-earth-surface-temperature-data"
+        self.path = "./kaggle"
+        self.csv_file = "./kaggle/GlobalLandTemperaturesByCountry.csv"
+
+    def download_dataset(self):
+        kaggle.api.authenticate()
+        kaggle.api.dataset_download_files(
+            self.dataset, path=self.path, force=True, unzip=True
+        )
+
+    def open_files(self):
+        df_global_temperatures_country = pd.read_csv(self.csv_file)
+        return df_global_temperatures_country
+
+    def read_files(self, second_try=False):
+        try:
+            df_country = self.open_files()
+
+        except (FileNotFoundError, OSError):
+            # if files don't exist or are locked, download them
+            try:
+                self.download_dataset()
+                df_country = self.open_files()
+            except kaggle.rest.ApiException as ex:
+                if ex.status == 403:
+                    # Forbidden
+                    sys.exit(
+                        "No files found locally and download forbidden, check credentials and url"
+                    )
+                elif ex.status == 401:
+                    # Unauthorized
+                    sys.exit(
+                        "No files found locally and no credentials found, check credentials"
+                    )
+                elif ex.status == 404:
+                    # Not Found
+                    sys.exit("No files found locally and online, check url")
+                elif ex.status == 408:
+                    # Timed out
+                    if second_try:
+                        sys.exit(
+                            "No files found locally and connection to kaggle timed out"
+                        )
+                    else:
+                        self.read_files(second_try=True)
+                else:
+                    sys.exit(
+                        f"No files found locally and kaggle returned status {ex.status}"
+                    )
+            except (FileNotFoundError, OSError) as ex:
+                sys.exit(f"Downloaded files not found or corrupted {type(ex)}")
+
+        df_country = df_country.dropna()
+
+        # Extract the years from df['dt']
+        years = np.unique(df_country["dt"].apply(lambda x: x[:4]))
+
+        # Extract the countries from df
+        countries = df_country["Country"].unique()
+
+        return df_country, years, countries
